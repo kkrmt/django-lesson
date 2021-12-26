3587df6dedeb27aaae0aeb38a768ca7a44e5c2da
kkrmt <kurimoto.kei.721+kkrmt@gmail.com>
2021-12-27 00:12:55 +0900

    GH-4: add category list

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, resolve_url, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView, CreateView, DetailView, UpdateView, DeleteView, ListView
-from .models import Post, Like
+from .models import Post, Like, Category
 from django.urls import reverse_lazy
 from .forms import PostForm, LoginForm, SignUpForm
 from django.contrib import messages
@@ -100,3 +100,9 @@ def Like_add(request, post_id):
   like.save()
   messages.success(request, 'お気に入り登録しました')
   return redirect('myapp:post_detail', post.id)
+
+
+class CategoryList(ListView):
+  model = Category
+  def get_queryset(self):
+    return Category.objects.all().order_by('id')
