359ea1aa8e0bdfdb6bb70bff490373811c98237b
kkrmt <kurimoto.kei.721+kkrmt@gmail.com>
2021-12-26 23:39:26 +0900

    GH-4: add category model

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Post, Like
+from .models import Post, Like, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-  list_display = ('id', 'author', 'title', 'created_at')
-  list_display_links = ('title',)
+  list_display = ('id', 'category', 'author', 'title', 'created_at')
+  list_display_links = ('title', 'category')
   ordering = ('-created_at',)
 
 @admin.register(Like)
@@ -12,3 +12,9 @@ class LikeAdmin(admin.ModelAdmin):
   list_display = ('id', 'post', 'user')
   list_display_links = ('id',)
   ordering = ('-id',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'name_en', 'created_at')
+  list_display_links = ('name',)
+  ordering = ('-created_at',)
